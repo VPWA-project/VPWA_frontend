332bed4b6bcaac8efcbd83408bd6dd8b00af0a9c
Adam Bublavý <bublavy.adam@gmail.com>
2022-03-15 15:19:59 +0100

    Refactor: extracted RightDrawer from MainLayout

---
+++ src/components/RightDrawer.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-list>
+    <q-item>
+      <q-item-section>
+        <q-item-label class="text-grey-7">Online</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <UserBanner
+      firstname="Frido"
+      lastname="Herak"
+      nickname="cigan123"
+      status="ONLINE"
+    />
+
+    <q-item>
+      <q-item-section>
+        <q-item-label class="text-grey-7">Offline</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <UserBanner
+      firstname="Frido"
+      lastname="Herak"
+      nickname="cigan123"
+      status="OFFLINE"
+    />
+  </q-list>
+</template>
+
+<script lang="ts">
+import UserBanner from './UserBanner.vue';
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    UserBanner,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,38 +7,17 @@
       />
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="state.isLeftDrawerOpen" show-if-above bordered>
       <LeftDrawer />
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <q-list>
-        <q-item>
-          <q-item-section>
-            <q-item-label class="text-grey-7">Online</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <UserBanner
-          firstname="Frido"
-          lastname="Herak"
-          nickname="cigan123"
-          status="ONLINE"
-        />
-
-        <q-item>
-          <q-item-section>
-            <q-item-label class="text-grey-7">Offline</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <UserBanner
-          firstname="Frido"
-          lastname="Herak"
-          nickname="cigan123"
-          status="OFFLINE"
-        />
-      </q-list>
+    <q-drawer
+      show-if-above
+      v-model="state.isrightDrawerOpen"
+      side="right"
+      bordered
+    >
+      <RightDrawer />
     </q-drawer>
 
     <q-page-container>
@@ -52,69 +31,37 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, reactive } from 'vue';
 import { useStore } from '../store';
-import ChannelLink from 'src/components/ChannelLink.vue';
-import SearchChannels from 'src/components/SearchChannels.vue';
-import {
-  Channel,
-  InvitationState,
-  InvitationInfo,
-} from 'src/store/channels/state';
-import { UserStatus } from 'src/store/user/state';
 import MessageForm from 'src/components/MessageForm.vue';
-import UserBanner from '../components/UserBanner.vue';
 import Header from 'src/components/Header.vue';
-import UserMenu from 'src/components/UserMenu.vue';
 import LeftDrawer from '../components/LeftDrawer.vue';
+import RightDrawer from 'src/components/RightDrawer.vue';
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
     MessageForm,
-    UserBanner,
     Header,
     LeftDrawer,
+    RightDrawer,
   },
 
   setup() {
-    const leftDrawerOpen = ref(false);
-    const rightDrawerOpen = ref(false);
-    const browseChannelsOpen = ref(false);
-    const allowOnlyMentions = ref(true);
-
-    const btnIcon = ref('expand_more');
-
     const $store = useStore();
 
+    const state = reactive({
+      isLeftDrawerOpen: false,
+      isrightDrawerOpen: false,
+    });
+
     return {
-      btnIcon,
-      message: '',
-      leftDrawerOpen,
-      rightDrawerOpen,
-      browseChannelsOpen,
-      allowOnlyMentions,
-      confirm: ref(false),
-      user: computed(() => $store.state.user),
-      UserStatus,
-      InvitationState,
-
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-
-      /*
-      userGetter: computed<string>(() => {
-        return $store.getters.userGetter;
-      }),
-      */
-      activeChannel: computed(() => {
-        return $store.state.channels.activeChannel;
-      }),
-      invitations: computed(() => {
-        return $store.state.channels.invitations;
-      }),
+      state,
+      toggleLeftDrawer: () =>
+        (state.isLeftDrawerOpen = !state.isLeftDrawerOpen),
+      activeChannel: computed(() => $store.state.channels.activeChannel),
+      invitations: computed(() => $store.state.channels.invitations),
     };
   },
 });
