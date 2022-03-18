406ce9da60ed60d1751484986914a92447c18c31
Adam Bublavý <bublavy.adam@gmail.com>
2022-03-18 11:38:48 +0100

    Fix: Wrong position of arrow in UserBanner

--- src/components/UserBanner.vue
+++ src/components/UserBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-item class="flex-row">
+  <q-item class="full-width">
     <q-item-section avatar>
       <q-avatar rounded color="primary" text-color="white">
         {{ nameInitials }}
