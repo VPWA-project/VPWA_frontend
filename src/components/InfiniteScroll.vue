c554486b062a8d9f6f73fe03bd0a2358408c07e1
Adam Fagan <adam.fagann@gmail.com>
2022-03-19 23:12:06 +0100

    Added: Tagged message

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -7,13 +7,23 @@
         </div>
       </template>
 
-      <div v-for="(message, index) in items" :key="index">
+      <div v-for="(message, index) in selectedChannelMessages" :key="index">
         <div>
           <q-chat-message
+            v-if="message.tag"
             push
             name="me"
             avatar="https://cdn.quasar.dev/img/avatar1.jpg"
-            :text="[message]"
+            :text="[message.message]"
+            bg-color="red"
+          />
+          <q-chat-message
+            v-if="!message.tag"
+            push
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+            :text="[message.message]"
+            bg-color="green"
           />
           <q-popup-proxy>
             <div>
@@ -31,7 +41,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
   // props: {
@@ -40,12 +51,59 @@ export default defineComponent({
 
   setup() {
     const items = ref(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+    const router = useRouter();
+    const route = useRoute();
+    const channelId = ref(1);
+
+    watch(
+      () => route.params.id,
+      (id) => (channelId.value = id as unknown as number)
+    );
+
+    const channelsMessages = ref([
+      {
+        id: 1,
+        messages: [
+          { tag: true, message: 'Hello' },
+          { tag: false, message: 'Good morning' },
+          { tag: false, message: 'Bye' },
+          { tag: true, message: 'How are you ?' },
+        ],
+      },
+      {
+        id: 2,
+        messages: [
+          { tag: false, message: 'Hi, my name is Jozko' },
+          { tag: true, message: 'Hello, Jozko !' },
+          { tag: false, message: 'How is it going ?' },
+          { tag: false, message: 'Pretty well' },
+        ],
+      },
+    ]);
+
+    var selectedChannel = channelsMessages.value.find(
+      ({ id }) => id == channelId.value
+    );
+
+    let selectedChannelMessages = selectedChannel?.messages.map((a) => a);
 
     return {
       items,
+      channelsMessages,
+      selectedChannelMessages,
+
       onLoad: (_: number, done: (stop: boolean | undefined) => void) => {
+        //console.log(result);
         setTimeout(() => {
-          items.value.splice(0, 0, 'a', 'b', 'c', 'd', 'e');
+          selectedChannelMessages?.splice(
+            0,
+            0,
+            { tag: false, message: 'a' },
+            { tag: false, message: 'b' },
+            { tag: false, message: 'c' },
+            { tag: false, message: 'd' },
+            { tag: false, message: 'e' }
+          );
           done(false);
         }, 2000);
       },
