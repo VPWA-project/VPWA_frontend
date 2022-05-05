62495ff5bb482dc1bed6717a7915c0f92df9ee44
Adam Bublavý <bublavy.adam@gmail.com>
2022-05-05 15:02:54 +0200

    Refactor: MessageForm commands

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -30,12 +30,8 @@ import { useStore } from 'src/store';
 import { useRoute, useRouter } from 'vue-router';
 import {
   Channel,
-  ChannelType,
-  CreateChannelRequest,
-  KickType,
-  User,
 } from 'src/contracts';
-import { Notify } from 'quasar';
+import { commandService } from 'src/services';
 
 export default defineComponent({
   name: 'MessageForm',
@@ -70,192 +66,6 @@ export default defineComponent({
       if (!state.message) state.sendedLastEdit = false;
     };
 
-    const notifyUser = (message: string, type: string, color: string) => {
-      Notify.create({
-        message,
-        color: color,
-        textColor: 'black',
-        type: type,
-        position: 'bottom',
-      });
-    };
-
-    const notifyUserPositive = (message: string) =>
-      notifyUser(message, 'positive', 'grey-2');
-    const notifyUserNegative = (message: string) =>
-      notifyUser(message, 'negative', 'red-2');
-
-    const processJoinCommand = async (args: string[]) => {
-      const [channelName, channelType = 'PUBLIC', rest] = args;
-
-      if (
-        !rest &&
-        (channelType.toUpperCase() === ChannelType.Public ||
-          channelType.toUpperCase() === ChannelType.Private)
-      ) {
-        await $store
-          .dispatch('createChannel/create', {
-            name: channelName,
-            type: channelType,
-            invitations: undefined,
-          } as CreateChannelRequest)
-          .then(() =>
-            notifyUserPositive(
-              `The channel ${channelName} was created successfully`
-            )
-          )
-          .catch(async () => {
-            const channel = (await $store.dispatch(
-              'channels_v2/getChannel',
-              channelName
-            )) as Channel | null;
-
-            if (channel)
-              await $store
-                .dispatch('channels_v2/joinChannel', channel.id)
-                .then(() =>
-                  notifyUserPositive(
-                    `Successfully joined to the channel ${channelName}`
-                  )
-                )
-                .catch(() => notifyUserNegative('Unexpected error'));
-          });
-      } else
-        notifyUserNegative(
-          'The syntax of the command is incorrect. Use: /join channelName [private]'
-        );
-    };
-
-    const processCancelCommand = async (
-      args: string[],
-      activeChannel: Channel | null
-    ) => {
-      if (args.length) {
-        notifyUserNegative(
-          'The syntax of the command is incorrect. Use: /cancel'
-        );
-        return;
-      }
-      if (!activeChannel) {
-        notifyUserNegative('No channel was selected');
-        return;
-      }
-
-      const channelname = activeChannel.name;
-
-      await $store
-        .dispatch('channels_v2/leaveChannel', activeChannel.name)
-        .then(async () => {
-          notifyUserPositive(`Channel ${channelname} left successfully`);
-          await router.push({ name: 'home' });
-        })
-        .catch(() => notifyUserNegative('Unexpected error'));
-    };
-
-    const processQuitCommand = async (
-      args: string[],
-      activeChannel: Channel | null,
-      amIChannelAdmin: boolean
-    ) => {
-      if (args.length) {
-        notifyUserNegative(
-          'The syntax of the command is incorrect. Use: /quit'
-        );
-        return;
-      }
-      if (!activeChannel) {
-        notifyUserNegative('No channel was selected');
-        return;
-      }
-      if (!amIChannelAdmin) {
-        notifyUserNegative('You are not admin of the channel');
-        return;
-      }
-
-      const channelName = activeChannel.name;
-
-      await $store
-        .dispatch('channels_v2/leaveChannel', activeChannel.name)
-        .then(async () => {
-          notifyUserPositive(`Channel ${channelName} left successfully`);
-          await router.push({ name: 'home' });
-        })
-        .catch(() => notifyUserNegative('Unexpected error'));
-    };
-
-    const processKickRevokeCommand = async (
-      command: string,
-      args: string[],
-      activeChannel: Channel | null
-    ) => {
-      if (args.length !== 1) {
-        notifyUserNegative(
-          `The syntax of the command is incorrect. Use: ${command} nickname`
-        );
-        return;
-      }
-      if (!activeChannel) {
-        notifyUserNegative('No channel was selected');
-        return;
-      }
-      const nickname = args[0];
-
-      const userToBeKicked = (await $store.dispatch(
-        'channels_v2/getUserByNicknameFromActiveChannelStore',
-        nickname
-      )) as User | undefined;
-
-      if (userToBeKicked)
-        await $store
-          .dispatch('channels_v2/kickUser', {
-            channelName: activeChannel.name,
-            userId: userToBeKicked.id,
-            method: command === '/kick' ? KickType.Kick : KickType.Revoke,
-          })
-          .then(() =>
-            notifyUserPositive(
-              `User ${nickname} was ${
-                command === '/kick' ? 'kicked' : 'revoked'
-              } successfully`
-            )
-          )
-          .catch(() => notifyUserNegative('Unexpected error'));
-      else notifyUserNegative(`User ${nickname} was not found`);
-    };
-
-    const processInviteCommand = async (
-      args: string[],
-      activeChannel: Channel | null,
-      amIChannelAdmin: boolean
-    ) => {
-      if (args.length !== 1) {
-        notifyUserNegative(
-          'The syntax of the command is incorrect. Use: /invite nickname'
-        );
-        return;
-      }
-      if (!activeChannel) {
-        notifyUserNegative('No channel was selected');
-        return;
-      }
-      if (activeChannel.type === ChannelType.Private && !amIChannelAdmin) {
-        notifyUserNegative('You are not admin of the channel');
-        return;
-      }
-
-      const nickname = args[0];
-
-      await $store
-        .dispatch('invitations/inviteByNickname', {
-          channelId: activeChannel.id,
-          nicknames: args,
-        })
-        .then(() =>
-          notifyUserPositive(`User ${nickname} was invited successfully`)
-        )
-        .catch(() => notifyUserNegative('Unexpected error'));
-    };
-
     const handleSubmit = async () => {
       if (!state.message) return;
 
@@ -265,25 +75,38 @@ export default defineComponent({
         const command = words[0];
         const args = words.slice(1);
 
-        if (command === '/join') await processJoinCommand(args);
+        if (command === '/join')
+          await commandService.processJoinCommand($store, args);
         else if (command === '/cancel')
-          await processCancelCommand(args, activeChannel.value);
+          await commandService.processCancelCommand(
+            $store,
+            router,
+            args,
+            activeChannel.value
+          );
         else if (command === '/quit')
-          await processQuitCommand(
+          await commandService.processQuitCommand(
+            $store,
+            router,
             args,
             activeChannel.value,
             amIChannelAdmin.value
           );
         else if (command === '/kick' || command === '/revoke')
-          await processKickRevokeCommand(command, args, activeChannel.value);
+          await commandService.processKickRevokeCommand(
+            $store,
+            command,
+            args,
+            activeChannel.value
+          );
         else if (command === '/invite')
-          await processInviteCommand(
+          await commandService.processInviteCommand(
+            $store,
             args,
             activeChannel.value,
             amIChannelAdmin.value
           );
       } else {
-        // TODO: parse tags
         const tags = state.message
           .split(' ')
           .filter((word) => word.startsWith('@'))
--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,4 @@ export { default as channelService } from './ChannelService';
 export { default as activityService } from './ActivityService';
 export { default as invitationService } from './InvitationService';
 export { default as invitationManager } from './InvitationManager';
+export { default as commandService } from './CommandService';
